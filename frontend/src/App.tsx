9ab41fb6347a1e37b9b3c1c541cd43a92651fd2b
guzmandp <guzmandp@gmail.com>
2023-05-12 20:11:22 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import NotificationButton from './components/notificationButton'
 
 function App() {
 
   return (
     <>
       <h1>Hello World</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
     </>
   )
 }
