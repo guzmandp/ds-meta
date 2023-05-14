917e7eaa46d577d2372f63725da839c112c669ea
guzmandp <guzmandp@gmail.com>
2023-05-13 21:53:18 -0300

    Date select

--- backend/src/main/java/com/guzmandp/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/guzmandp/dsmeta/services/SaleService.java
@@ -1,9 +1,14 @@
 package com.guzmandp.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.guzmandp.dsmeta.entities.Sale;
 import com.guzmandp.dsmeta.repositories.SaleRepository;
@@ -14,7 +19,17 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(
+			String minDate, 
+			String maxDate,
+			Pageable pageable) {
+		
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
