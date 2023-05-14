a70475ea4de00f716e3f454a5c70ba45806b7889
guzmandp <guzmandp@gmail.com>
2023-05-13 22:46:41 -0300

    Twilio SMS

--- backend/src/main/java/com/guzmandp/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/guzmandp/dsmeta/services/SaleService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.guzmandp.dsmeta.entities.Sale;
 import com.guzmandp.dsmeta.repositories.SaleRepository;
